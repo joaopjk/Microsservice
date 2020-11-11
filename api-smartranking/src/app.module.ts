125e598bf18692f59b89988029e3905be2fd273d
João <joaopjk48@gmail.com>
2020-11-11 20:30:07 -0300

    Classe Jogadores implementando o MongoDB

--- api-smartranking/src/app.module.ts
+++ api-smartranking/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import {MongooseModule} from '@nestjs/mongoose'
 @Module({
-  imports: [JogadoresModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://admin:KvsvcZl6NCcwpA0R@cluster0.mj8qo.mongodb.net/smartranking?retryWrites=true&w=majority',
+  {useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology: true, useFindAndModify: false}),JogadoresModule],
   controllers: [],
   providers: [],
 })
